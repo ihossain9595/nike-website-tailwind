997f68a4ad4fd89599ef3de9da2200fb444e51b7
Ismail Hossain <ismail.9595@gmail.com>
2024-06-14 19:03:28 +0600

    create super quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ label, iconUrl }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
-      {label} <img src={iconUrl} alt="Right arrow icon" className="ml-2 rounded-full w-5 h-5" />
+      {label} {iconUrl && <img src={iconUrl} alt="Right arrow icon" className="ml-2 rounded-full w-5 h-5" />}
     </button>
   );
 };
