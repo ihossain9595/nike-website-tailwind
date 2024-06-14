5a220b79b332e337951eaf2bfedf4e25d2a3a01f
Ismail Hossain <ismail.9595@gmail.com>
2024-06-14 23:52:01 +0600

    fix font issue on hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,8 +11,8 @@ const Hero = () => {
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm: text-[72px] max-sm:leading-[82] leading-none font-bold">
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span> <br />
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
